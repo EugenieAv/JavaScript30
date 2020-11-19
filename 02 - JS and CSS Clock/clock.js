87f2bb344f7b8208f4e37d548d441a34762533be
Eugenie Avedikian <eugenieavedikian@gmail.com>
2020-11-19 15:59:55 +0100

    done

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -1,1 +1,19 @@
-console.log ('bla')
+const secondNeedle = document.querySelector(".second-hand");
+const minutNeedle = document.querySelector(".min-hand");
+const hourNeedle = document.querySelector(".hour-hand");
+
+const setDate = () => {
+  const now = new Date;
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondsDegree = seconds / 60 * 360;
+  const minutesDegree = minutes / 60 * 360;
+  const hoursDegree = hours / 12 *360;
+
+  secondNeedle.style.transform = `rotate(${secondsDegree + 90}deg)`;
+  minutNeedle.style.transform = `rotate(${minutesDegree + 90}deg)`;
+  hourNeedle.style.transform = `rotate(${hoursDegree + 90}deg)`
+  // console.log(seconds);
+}
+setInterval(setDate, 1000)
